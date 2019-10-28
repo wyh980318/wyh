da41b907995875a0e6a5af128095a449d7f72dbf
wyh980318 <1652498197@qq.com>
2019-10-28 14:48:03 +0800

    xx

---
+++ springbootmybatis/src/main/java/com/example/springbootmybatis/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.example.springbootmybatis.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component
+public interface UserDao {
+}
