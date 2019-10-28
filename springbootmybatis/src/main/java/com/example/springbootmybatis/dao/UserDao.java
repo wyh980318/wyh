22c69bda8f9361852e0bf470826a47d0073b2287
wyh980318 <1652498197@qq.com>
2019-10-28 15:21:36 +0800

    初始化项目

--- springbootmybatis/src/main/java/com/example/springbootmybatis/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootmybatis.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
-
-@Mapper
-@Component
-public interface UserDao {
-}
